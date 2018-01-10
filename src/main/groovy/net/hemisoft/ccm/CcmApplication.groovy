02ed4071e60f37346df182fda86f669810bac5de
Michael Hegner <michaelhegner77@gmail.com>
2018-01-10 20:10:45 +0100

    New Blank Project
    New Blank Project

---
+++ src/main/groovy/net/hemisoft/ccm/CcmApplication.groovy
@@ -0,0 +1,12 @@
+package net.hemisoft.ccm
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class CcmApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run CcmApplication, args
+	}
+}
