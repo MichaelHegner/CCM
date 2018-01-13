7be827f27cd8228c98f7f0cde84a53a56a8f96f7
Michael Hegner <michaelhegner77@gmail.com>
2018-01-13 10:42:11 +0100

    Nomin Automap Fix

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 ## Trouble Shootings
 https://discuss.gradle.org/t/specifying-jre-in-eclipse-using-buildship-does-not-work/21474/4
 https://stackoverflow.com/questions/48216259/nomin-mapper-framework-for-groovy/48217572?noredirect=1#comment83445775_48217572
+http://nomin.sourceforge.net/introspectors.html
--- src/main/resources/net/hemisoft/ccm/repository/coinmarketcap2coin.groovy
+++ src/main/resources/net/hemisoft/ccm/repository/coinmarketcap2coin.groovy
@@ -4,20 +4,10 @@ import net.hemisoft.ccm.domain.CoinsOnMarketPlace
 import net.hemisoft.ccm.porter.Coin
 
 mappingFor a: CoinsOnMarketPlace, b: Coin
-// automap()
+introspector exploding 
+automap()
 
 a.coin.coinId		=		b.coinId
 a.coin.name			=		b.name
 a.coin.symbol		=		b.symbol
 a.coin.rank			=		b.rank
-a.priceUSD 			= 		b.priceUSD
-a.priceBTC 			= 		b.priceBTC
-a.volume24hUSD 		= 		b.volume24hUSD
-a.marketCapUSD 		= 		b.marketCapUSD
-a.availableSupply 	= 		b.availableSupply
-a.totalSupply 		= 		b.totalSupply
-a.maxSupply 		= 		b.maxSupply
-a.changePercent1h 	= 		b.changePercent1h
-a.changePercent24h 	= 		b.changePercent24h
-a.changePercent7d 	= 		b.changePercent7d
-a.lastUpdate 		= 		b.lastUpdate
