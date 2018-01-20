f2a5ca18b8d1ba9aa4d718170dc0e4eaf062d048
Michael Hegner <michaelhegner77@gmail.com>
2018-01-20 17:03:51 +0100

    Use ToString for MarketPlace

--- src/main/groovy/net/hemisoft/ccm/domain/MarketPlace.groovy
+++ src/main/groovy/net/hemisoft/ccm/domain/MarketPlace.groovy
@@ -6,9 +6,11 @@ import javax.persistence.GeneratedValue
 import javax.persistence.Id
 
 import groovy.transform.EqualsAndHashCode
+import groovy.transform.ToString
 
 @Entity
 @EqualsAndHashCode(includes = ["name"])
+@ToString
 class MarketPlace {
 	@Id @GeneratedValue		Long id
 	@Column(unique=true)	String name
