63c5ad477b8c58a57662e48255169bbdd45972f6
Michael Hegner <michaelhegner77@gmail.com>
2018-01-21 17:06:54 +0100

    Adding Scheduler and Keep Application alive until someone press enter on console

--- src/main/groovy/net/hemisoft/ccm/CcmApplication.groovy
+++ src/main/groovy/net/hemisoft/ccm/CcmApplication.groovy
@@ -2,11 +2,21 @@ package net.hemisoft.ccm
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Import
+
+import net.hemisoft.ccm.domain.DomainConfig
+import net.hemisoft.ccm.porter.PorterConfig
+import net.hemisoft.ccm.repository.RepositoryConfig
 
 @SpringBootApplication
 class CcmApplication {
 
 	static void main(String[] args) {
-		SpringApplication.run CcmApplication, args
+		def ctx = SpringApplication.run CcmApplication, args
+		def scanner = new Scanner(System.in);
+		print 'Press <Return> to quit program'
+		scanner.nextLine()
+		scanner.close()
+		ctx.close()
 	}
 }
---
+++ src/main/groovy/net/hemisoft/ccm/ScheduledConfiguration.groovy
@@ -0,0 +1,21 @@
+package net.hemisoft.ccm;
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.messaging.MessageChannel
+import org.springframework.messaging.support.MessageBuilder
+import org.springframework.scheduling.annotation.EnableScheduling
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableScheduling
+class ScheduledConfiguration {
+	@Autowired @Qualifier("coinmarketcap.request.channel")
+	MessageChannel requestChannel
+	
+	@Scheduled(fixedRate = 10000l)
+	public void reportCurrentTime() {
+		requestChannel.send(MessageBuilder.withPayload("").build())
+	}
+}
